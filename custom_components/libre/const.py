b1e71cec9ff4d491f7231f613d9ad477ea56e1c6
JRSmile <jan@behead.de>
2023-05-29 15:28:05 +0200

    added logger

--- custom_components/libre/const.py
+++ custom_components/libre/const.py
@@ -1,5 +1,6 @@
 """Constants for the Libre integration."""
-
+from logging import Logger, getLogger
 # This is the internal name of the integration, it should also match the directory
 # name for the integration.
 DOMAIN = "libre"
+LOGGER: Logger = getLogger(__package__)
